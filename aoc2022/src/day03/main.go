991c65020f85dc22df6b997e2b01c24e1a72da14
Kevin Field <kevinfield@m-tqk06r0x3g.lan>
2022-12-07 20:33:57 +0000

    completed day03

--- aoc2022/src/day03/main.go
+++ aoc2022/src/day03/main.go
@@ -8,8 +8,15 @@ func main() {
 	inputFile, _ := inputFlags()
 	// pull in puzzle input
 	puzzleInput, _ := readFile(inputFile)
-	var finalSlice []byte
 
+	p1result := part1(puzzleInput)
+	p2result := part2(puzzleInput)
+
+	fmt.Println("part 1 = ", p1result)
+	fmt.Println("part 2 =", p2result)
+}
+
+func alphabetCalc(finalSlice []byte) int {
 	// create alphabet/value slice and convert to byte
 	var alphaSlice []byte
 	for ch := 'a'; ch <= 'z'; ch++ {
@@ -19,23 +26,6 @@ func main() {
 		alphaSlice = append(alphaSlice, byte(chUp))
 	}
 
-	// loop through and compare the first half and second half and grab the match
-	for i := 0; i < len(puzzleInput); i++ {
-		lengthSlice := len(puzzleInput[i])
-		halfSlice := lengthSlice / 2
-	innerloop:
-		for j := 0; j < halfSlice; j++ {
-			for k := halfSlice; k < lengthSlice; k++ {
-				if puzzleInput[i][j] == puzzleInput[i][k] {
-					finalSlice = append(finalSlice, puzzleInput[i][j])
-					break innerloop
-				}
-			}
-
-		}
-	}
-
-	//loop through finalSlice to get values from alphaSlice
 	var totalPriority int
 	for l := 0; l < len(alphaSlice); l++ {
 		for m := 0; m < len(finalSlice); m++ {
@@ -44,5 +34,55 @@ func main() {
 			}
 		}
 	}
-	fmt.Println(totalPriority)
+	return totalPriority
+}
+
+func part1(p1Input []string) int {
+	// loop through and compare the first half and second half and grab the match
+	var p1finalSlice []byte
+	for i := 0; i < len(p1Input); i++ {
+		lengthSlice := len(p1Input[i])
+		halfSlice := lengthSlice / 2
+	innerloop:
+		for j := 0; j < halfSlice; j++ {
+			for k := halfSlice; k < lengthSlice; k++ {
+				if p1Input[i][j] == p1Input[i][k] {
+					p1finalSlice = append(p1finalSlice, p1Input[i][j])
+					break innerloop
+				}
+			}
+
+		}
+	}
+	return alphabetCalc(p1finalSlice)
+}
+
+func part2(p2Input []string) int {
+	//var elfPriority int
+	var p2finalSlice []byte
+
+	for i := 0; i < len(p2Input); {
+		elf1 := p2Input[i]
+		elf2 := p2Input[i+1]
+		elf3 := p2Input[i+2]
+	outerloop:
+		for j := 0; j < len(elf1); j++ {
+			for k := 0; k < len(elf2); k++ {
+				if elf1[j] == elf2[k] {
+					for l := 0; l < len(elf3); l++ {
+						if elf1[j] == elf3[l] {
+							p2finalSlice = append(p2finalSlice, elf2[k])
+							break outerloop
+						}
+					}
+
+				}
+			}
+		}
+
+		i += 3
+	}
+
+	return alphabetCalc(p2finalSlice)
+
 }
